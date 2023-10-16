6a30f2db6ab67ff2bde8f0bfaa9ca33822823121
mohith <147404644+mohithgpawar@users.noreply.github.com>
2023-10-16 19:09:27 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({ 
   selector: 'app-component',
   template: `
-    <h1>Hello World</h1>
+    <h1>Hello World zillion</h1>
   `
 })
 export class AppComponent {
